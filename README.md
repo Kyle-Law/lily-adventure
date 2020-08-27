c3f6194633da63cd2e08fa8c34b98588c12d759f
kyle-law <junwei_1121@hotmail.com>
2020-08-27 09:13:45 +0800

    Refine: Refine audio's volume and fix testing bugs, added README

---
+++ README.md
@@ -0,0 +1,55 @@
+# Brocollily
+
+> A simple game built by Phaser.js
+
+![image](https://user-images.githubusercontent.com/55923773/91371095-af9cdd00-e842-11ea-9e94-0d748e34eba3.png)
+
+## Live Version
+
+View the live version [HERE](https://lily-adventure.herokuapp.com/)
+
+## Built With
+
+- JavaScript
+- Phaser JS
+- Jest for Testing
+
+## Deployment
+
+1. Clone the repo
+2. run `npm run i` and `npm run start`
+3. Open `http://localhost:8080/`
+4. Enjoy :)
+
+## Testing
+
+1. Run `npm i`
+2. Run `npm test`
+3. `24 passed, 24 total` will be shown. :)
+
+## Authors
+
+👤 **Kyle Law**
+
+- Github: [@Kyle-Law](https://github.com/Kyle-Law)
+- Twitter: [@Kyle-Law](https://twitter.com/ZhunKhing)
+- Linkedin: [Kyle law](https://www.linkedin.com/in/kyle-lawzhunkhing/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Kyle-Law/micro-reddit/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse
+- The Odin Project
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
--- src/Scenes/GameOverScene.js
+++ src/Scenes/GameOverScene.js
@@ -1,34 +1,34 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-restricted-globals */
-import Phaser from 'phaser';
-import Button from '../Objects/Button';
-import Player from '../Classes/Player';
-import config from '../Config/config';
-import API from '../Objects/API';
-import blue_button02 from '../assets/ui/blue_button02.png';
+import Phaser from "phaser";
+import Button from "../Objects/Button";
+import Player from "../Classes/Player";
+import config from "../Config/config";
+import API from "../Objects/API";
+import blue_button02 from "../assets/ui/blue_button02.png";
 
 export default class GameOverScene extends Phaser.Scene {
   constructor() {
-    super('GameOver');
+    super("GameOver");
   }
 
   init() {
     this.model = this.sys.game.globals.model;
-    this.gameScene = this.scene.get('Main');
+    this.gameScene = this.scene.get("Main");
     this.gameScene.registry.destroy(); // destroy registry
     this.gameScene.events.off(); // disable all active events
     // this.gameScene.scene.restart();// restart current scene
   }
 
   preload() {
-    this.load.image('gameOverTitle', 'src/assets/ui/blue_button02.png');
+    this.load.image("gameOverTitle", "src/assets/ui/blue_button02.png");
   }
 
   create() {
     this.sys.game.globals.bgMusic.stop();
     const user = this.sys.game.globals.model.userName;
 
-    const score = localStorage.getItem('score');
+    const score = localStorage.getItem("score");
     localStorage.clear();
     API.postScores(user, score);
 
@@ -37,40 +37,49 @@ export default class GameOverScene extends Phaser.Scene {
       250,
       `Hello ${user}, your score is: ${score}`,
       {
-        fontFamily: 'monospace',
+        fontFamily: "monospace",
         fontSize: 20,
-        fontStyle: 'bold',
-        color: '#ffffff',
-        align: 'center',
-      },
+        fontStyle: "bold",
+        color: "#ffffff",
+        align: "center",
+      }
     );
     this.player = new Player({
       scene: this,
       x: 380,
       y: 200,
-      texture: 'princess',
-      frame: 'princess_idle_1',
+      texture: "princess",
+      frame: "princess_idle_1",
     });
-    this.player.setTexture('items', 0);
+    this.player.setTexture("items", 0);
     this.player.setScale(2);
 
-    this.submitButton = new Button(
-      this,
-      300,
-      config.height / 2 + 100,
-      'blueButton1',
-      'blueButton2',
-      'Scores',
-      'LeaderBoard',
-    );
+    // this.submitButton = new Button(
+    //   this,
+    //   300,
+    //   config.height / 2 + 100,
+    //   'blueButton1',
+    //   'blueButton2',
+    //   'Scores',
+    //   'LeaderBoard',
+    // );
 
     const style = `background: url(${blue_button02}); cursor:pointer; color: #fff;`;
-    const menu = this.add.dom(500, 400, 'button', style, 'Menu');
+    const leaderBoard = this.add.dom(270, 400, "button", style, "Scores");
+    leaderBoard.scaleX = 3.5;
+    leaderBoard.scaleY = 2;
+    leaderBoard.addListener("click");
+
+    leaderBoard.on("click", () => {
+      this.scene.start("LeaderBoard");
+    });
+
+    const menu = this.add.dom(520, 400, "button", style, "Menu");
     menu.scaleX = 4;
     menu.scaleY = 2;
-    menu.addListener('click');
+    menu.addListener("click");
 
-    menu.on('click', () => {
+    menu.on("click", () => {
       history.go();
     });
   }
